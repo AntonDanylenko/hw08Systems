083e458e5c8b67aa76509e961865a7ed03a21d24
Anton Danylenko <adanylenko@stuy.edu>
2018-10-21 14:42:02 -0400

    s

---
+++ sieve.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "sieve.h"
+
+long sieve(long target){
+  long length = (long)(target * log(target) * 1.2);
+  char *list = malloc(length);
+
+  for(int x=3; x<length; x+=2){
+    list[x] = 'I';
+  }
+  for(int x=0; x<length; x+=2){
+    list[x] = 'C';
+  }
+  list[1] = 'C';
+  list[2] = 'P';
+
+  int count_prime = 1;
+  int i=3;
+  while(count_prime!=target && i<length){
+    if (list[i] == 'I'){
+      count_prime++;
+      list[i] = 'P';
+      for (int ii=2*i; ii<length; ii+=i){
+        list[ii] = 'C';
+      }
+    }
+    if (count_prime==target){
+      return i;
+    }
+    i+=2;
+  }
+}
